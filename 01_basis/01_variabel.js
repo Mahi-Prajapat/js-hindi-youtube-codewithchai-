147259247fcbec5b3cb7dd18d3002d73d27e697c
mahiprajapat683@gmail.com <164643209+Mahi-Prajapat@users.noreply.github.com>
2025-04-29 18:35:27 +0000

    variabel dataTypes

---
+++ 01_basis/01_variabel.js
@@ -0,0 +1,12 @@
+const accountId=144233
+let accountEmail="mahi@google.com"
+var accountPassword="15555"
+accountCity="Jaipur"
+
+accountEmail="no@hi.com"
+accountPassword="122121"
+accountCity="Bengaluru"
+/*prefer not to use var because of issue in 
+block scope and function scope*/
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
---
+++ 01_basis/02_dataTypes.js
@@ -0,0 +1,19 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3+3)// use the alter tag is available in (browser contextnode but im not use in now (we are using nodejs,not browser)
+console.log(3+3) //code readability should be high
+console.log("Mahi")
+let age=18
+let isLoggedIn=false
+let state;
+//number= 2to power 53
+//bigint
+//string=""
+// boolearn=true/false
+//num=standalone value
+//undefined= (value does't sign)
+//symbol=unique
+
+//object
+console.log(typeof "Mahi")
+console.log(typeof age);
