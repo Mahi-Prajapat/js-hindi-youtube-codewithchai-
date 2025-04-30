465fa5f04e5a48146b9e59b3902cc7ed019ddbd3
mahiprajapat683@gmail.com <164643209+Mahi-Prajapat@users.noreply.github.com>
2025-04-30 08:23:41 +0000

    conversionOperation

---
+++ 01_basis/03_conversionOperation.js
@@ -0,0 +1,18 @@
+let score="33abc"
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber=Number(score)
+//console.log(typeof valueInNumber);
+//console.log( valueInNumber);
+//"33" => 33
+//"33abc"=>NaN  not and number
+//true=1;false =0
+
+let isLoggedIn=""
+
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+let someNumber=33
+console.log(typeof someNumber);
